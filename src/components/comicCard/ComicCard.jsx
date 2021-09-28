520dc47a1f1404f119998c845257fb418fe6c75b
Paloma Borro <palomabs93@gmail.com>
2021-09-28 16:28:16 +0200

    retoques en vistas, render de ComicDetail no funciona

--- src/components/comicCard/ComicCard.jsx
+++ src/components/comicCard/ComicCard.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import "./ComicCard.css";
 
-export default function ComicCard({ name, image, issue_number, id }) {
+export default function ComicCard({ volume, image, issue_number, id }) {
   return (
     <div className="ComicCard">
       <Link to={`/new/${id}`}>
         <img className="ComicCard__image" src={image.original_url} alt="" />
         <div className="ComicCard__info">
-          <p className='ComicCard__info__name'>{name}</p>
-          <p className='ComicCard__info__issue'>Issue: {issue_number}</p>
+          <p className='ComicCard__info__name'>{volume.name}</p>
+          <p className='ComicCard__info__issue'>Issue #{issue_number}</p>
         </div>
       </Link>
     </div>
--- src/components/comicDetail/ComicDetail.jsx
+++ src/components/comicDetail/ComicDetail.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import { getComics } from '../services/ComicService';
+import './ComicDetail.css'
+
 
 export default function ComicDetail() {
-    const {comicId} = useParams
+    const {comicId} = useParams();
+    const [comic, setComic] = useState();
+
+    useEffect(() => {
+        getComics(comicId)
+          .then(res => {
+              setComic(res.results)
+          console.log(res.results)
+      })
+     }, [comicId]);
 
     return (
-        <div>
-            Detalle
+        <div className='ComicDetail'>
+            <img src={comic.image.original_url} alt="" />
+            <div className='ComicDetail__info'>
+                <h2>{comic.name} #{comic.issue_number}</h2>
+                <p>Published: {comic.cover_date}</p>
+                <p>{comic.description}</p>
+            </div>
         </div>
     );
 };
